7b7ebb02b5279eaeef38205879d00636d4a1f6fb
kurumi0913 <50101637+kurumi0913@users.noreply.github.com>
2024-05-22 16:17:28 +0800

    Update runserver.py

--- runserver.py
+++ runserver.py
@@ -6,10 +6,5 @@ from os import environ
 from LineWebhook import app
 
 if __name__ == '__main__':
-    HOST = environ.get('SERVER_HOST', 'localhost')
-    PORT = 64875
-    # try:
-    #     PORT = int(environ.get('SERVER_PORT', '5555'))
-    # except ValueError:
-    #     PORT = 5555
-    app.run(HOST, PORT)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
